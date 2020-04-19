b6d01cac9ac8781fe6214ea8e947952cf66b87ee
Nathan Johnson <nathancj224@gmail.com>
2020-04-19 05:56:22 -0400

    Rename main activity xml

--- trivia/app/src/main/java/com/hackernate/trivia/MainActivity.java
+++ trivia/app/src/main/java/com/hackernate/trivia/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_main);
 
         usernameText = findViewById(R.id.username);
 
