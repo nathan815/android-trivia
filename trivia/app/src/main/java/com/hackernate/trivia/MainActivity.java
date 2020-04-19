6532b766a0752eb0a92e6ad8d51f328911715acf
Nathan Johnson <nathancj224@gmail.com>
2020-04-19 13:09:36 -0400

    remove unused imports MainActivity

--- trivia/app/src/main/java/com/hackernate/trivia/MainActivity.java
+++ trivia/app/src/main/java/com/hackernate/trivia/MainActivity.java
@@ -1,14 +1,9 @@
 package com.hackernate.trivia;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +14,6 @@ import com.hackernate.trivia.data.Game;
 import com.hackernate.trivia.data.User;
 
 import java.util.UUID;
-import java.util.function.BiFunction;
 
 import io.socket.client.Socket;
 
