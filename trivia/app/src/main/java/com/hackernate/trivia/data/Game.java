5b6811e911f58cbf554c1a4a8f6bcfeaa2a0e65c
Nathan Johnson <nathancj224@gmail.com>
2020-04-19 10:42:19 -0400

    Add some logic methods to Game

--- trivia/app/src/main/java/com/hackernate/trivia/data/Game.java
+++ trivia/app/src/main/java/com/hackernate/trivia/data/Game.java
@@ -1,14 +1,39 @@
 package com.hackernate.trivia.data;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class Game {
 
     public String name;
+
+    @SerializedName("_id")
     public String id;
+    private String status;
+
     private List<User> players;
     public List<Question> questions;
 
+    public boolean isWaitingToStart() {
+        return status.equals("waiting");
+    }
+    public boolean hasStarted() {
+        return status.equals("inplay");
+    }
+
+
+    public List<User> getPlayers() {
+        return players;
+    }
+
+    public boolean canBeStarted() {
+        return players.size() >= 2;
+    }
+
+    public int playersCount()  {
+        return players.size();
+    }
     public void submitAnswer(String userId, int answerIndex) {
 
     }
