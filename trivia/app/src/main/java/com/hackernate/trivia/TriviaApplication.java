9d89536517d5fb418ee902731e90e88b084bb095
Nathan Johnson <nathancj224@gmail.com>
2020-04-19 04:51:31 -0400

    Add SharedPreferences to TriviaApplication

--- trivia/app/src/main/java/com/hackernate/trivia/TriviaApplication.java
+++ trivia/app/src/main/java/com/hackernate/trivia/TriviaApplication.java
@@ -1,6 +1,8 @@
 package com.hackernate.trivia;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 import java.net.URISyntaxException;
 
@@ -9,6 +11,8 @@ import io.socket.client.Socket;
 
 public class TriviaApplication extends Application {
     private Socket socket;
+    private SharedPreferences sharedPrefs;
+
     private static final String URL = "http://10.0.2.2:3000";
 
     @Override
@@ -19,9 +23,14 @@ public class TriviaApplication extends Application {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+        sharedPrefs = getSharedPreferences("application", Context.MODE_PRIVATE);
     }
 
     public Socket getSocket() {
         return socket;
+    }
+
+    public SharedPreferences getSharedPrefs() {
+        return sharedPrefs;
     }
 }
