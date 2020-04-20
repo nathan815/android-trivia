044f7202b96a2e157e6262323ebdef1c2f5bb6d4
Nathan Johnson <nathancj224@gmail.com>
2020-04-20 01:54:46 -0400

    Add User equals and hashCode

--- trivia/app/src/main/java/com/hackernate/trivia/data/User.java
+++ trivia/app/src/main/java/com/hackernate/trivia/data/User.java
@@ -3,6 +3,8 @@ package com.hackernate.trivia.data;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Objects;
+
 public class User {
     @SerializedName("_id")
     public String id;
@@ -11,6 +13,20 @@ public class User {
     public User(String id, String username) {
         this.id = id;
         this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) &&
+                username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
     }
 
     @Override
